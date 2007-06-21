03094e69798c514c928ce4af2be5756139d4b6bd
Etienne M. Gagnon <egagnon@j-meg.com>
2007-06-21 13:43:51 -0400

    Separate site link from surrounding parentheses.
    Signed-off-by: Etienne M. Gagnon <egagnon@j-meg.com>

--- src/org/sablecc/sablecc/alphabet/AlphabetMergeResult.java
+++ src/org/sablecc/sablecc/alphabet/AlphabetMergeResult.java
@@ -1,4 +1,4 @@
-/* This file is part of SableCC (http://sablecc.org/).
+/* This file is part of SableCC ( http://sablecc.org ).
  * 
  * See the NOTICE file distributed with this work for copyright information.
  *
