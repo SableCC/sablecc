3ecfa3aa58fc10d23a3f66606b1e710858a5dffc
Etienne M. Gagnon <egagnon@j-meg.com>
2007-06-23 22:14:51 -0400

    Add regression test.
    Signed-off-by: Etienne M. Gagnon <egagnon@j-meg.com>

---
+++ regression-tests/ticket25/TransitionBug.java
@@ -0,0 +1,36 @@
+/* This file is part of SableCC ( http://sablecc.org ).
+ * 
+ * See the NOTICE file distributed with this work for copyright information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ticket25;
+
+import org.junit.Test;
+import org.sablecc.sablecc.automaton.graph.State;
+import org.sablecc.sablecc.automaton.graph.Transition;
+
+public class TransitionBug {
+
+    @Test
+    public void bug() {
+
+        State<Integer> state = new State<Integer>("state");
+        Transition<Integer> transition = new Transition<Integer>(state, state,
+                null);
+
+        // Line that causes the bug.
+        transition.equals(transition);
+    }
+}
