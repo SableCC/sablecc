e6746061be5fd5c7595e3c7851d0e97f93c80ef5
Etienne M. Gagnon <egagnon@j-meg.com>
2007-06-25 20:51:34 -0400

    Add class to manage work sets.
    Signed-off-by: Etienne M. Gagnon <egagnon@j-meg.com>

---
+++ src/org/sablecc/sablecc/util/WorkSet.java
@@ -0,0 +1,58 @@
+/* This file is part of SableCC ( http://sablecc.org ).
+ * 
+ * See the NOTICE file distributed with this work for copyright information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.sablecc.sablecc.util;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.sablecc.sablecc.exception.InternalException;
+
+public class WorkSet<T> {
+
+    private final Set<T> done = new HashSet<T>();
+
+    private final LinkedHashSet<T> toDo = new LinkedHashSet<T>();
+
+    public boolean hasNext() {
+
+        return !this.toDo.isEmpty();
+    }
+
+    public T next() {
+
+        T next = this.toDo.iterator().next();
+
+        this.toDo.remove(next);
+        this.done.add(next);
+
+        return next;
+    }
+
+    public void add(
+            T element) {
+
+        if (element == null) {
+            throw new InternalException("element may not be null");
+        }
+
+        if (!this.done.contains(element)) {
+            this.toDo.add(element);
+        }
+    }
+}
