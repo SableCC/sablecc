4616f007fdee2680924d47bf35756399d47e3116
Raymond Audet <raymond.audet@gmail.com>
2007-06-27 12:24:03 -0400

    Add javadoc comments.
    Signed-off-by: Raymond Audet <raymond.audet@gmail.com>
    Signed-off-by: Etienne M. Gagnon <egagnon@j-meg.com>

--- src/org/sablecc/sablecc/util/WorkSet.java
+++ src/org/sablecc/sablecc/util/WorkSet.java
@@ -25,15 +25,28 @@ import org.sablecc.sablecc.exception.InternalException;
 
 public class WorkSet<T> {
 
+    /** The list of the elements already treated. */
     private final Set<T> done = new HashSet<T>();
 
+    /** The list of the elements left to be treated. */
     private final LinkedHashSet<T> toDo = new LinkedHashSet<T>();
 
+    /**
+     * Indicates if an element has a next one.
+     * 
+     * @return boolean a boolean value indicating whether the element has a next
+     *         one or not.
+     */
     public boolean hasNext() {
 
         return !this.toDo.isEmpty();
     }
 
+    /**
+     * Treats a new element, adding it to the done list.
+     * 
+     * @return T the treated element.
+     */
     public T next() {
 
         T next = this.toDo.iterator().next();
@@ -44,6 +57,14 @@ public class WorkSet<T> {
         return next;
     }
 
+    /**
+     * Adds a new element to be treated.
+     * 
+     * @param element
+     *            the new element to be treated.
+     * @throws InternalException
+     *             if the element is <code>null</code>.
+     */
     public void add(
             T element) {
 
