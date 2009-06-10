69c6fedba50791456c29b4f53fa341b52363e307
Etienne M. Gagnon <egagnon@j-meg.com>
2009-06-10 10:05:34 -0400

    Indent code.
    Signed-off-by: Etienne M. Gagnon <egagnon@j-meg.com>

--- src/org/sablecc/objectmacro/codegeneration/scala/CodeGenerationWalker.java
+++ src/org/sablecc/objectmacro/codegeneration/scala/CodeGenerationWalker.java
@@ -616,7 +616,8 @@ public class CodeGenerationWalker
             this.currentAfterOne.newParamInsert(string(node.getParamInsert()));
         }
         else if (this.currentBeforeMany != null) {
-            this.currentBeforeMany.newParamInsert(string(node.getParamInsert()));
+            this.currentBeforeMany
+                    .newParamInsert(string(node.getParamInsert()));
         }
         else if (this.currentAfterMany != null) {
             this.currentAfterMany.newParamInsert(string(node.getParamInsert()));
