6b9d33da90a68de87adf6d9393f1089e213f2747
Etienne M. Gagnon <egagnon@j-meg.com>
2011-02-11 12:46:50 -0500

    Add group class.
    Signed-off-by: Etienne M. Gagnon <egagnon@j-meg.com>

---
+++ src/org/sablecc/sablecc/core/Group.java
@@ -0,0 +1,62 @@
+/* This file is part of SableCC ( http://sablecc.org ).
+ *
+ * See the NOTICE file distributed with this work for copyright information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.sablecc.sablecc.core;
+
+import org.sablecc.exception.*;
+import org.sablecc.sablecc.syntax3.node.*;
+
+public class Group
+        implements NameDeclaration {
+
+    private final AGroup declaration;
+
+    private final Grammar grammar;
+
+    public Group(
+            AGroup declaration,
+            Grammar grammar) {
+
+        if (declaration == null) {
+            throw new InternalException("declaration may not be null");
+        }
+
+        if (grammar == null) {
+            throw new InternalException("grammar may not be null");
+        }
+
+        this.declaration = declaration;
+        grammar.addMapping(declaration, this);
+        this.grammar = grammar;
+    }
+
+    public TIdentifier getNameIdentifier() {
+
+        return this.declaration.getName();
+    }
+
+    public String getName() {
+
+        return getNameIdentifier().getText();
+    }
+
+    public String getNameType() {
+
+        return "group";
+    }
+
+}
