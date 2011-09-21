60cbf055e26b17abddaebc6f598fff139c011156
Jean Privat <jean@pryen.org>
2011-09-21 14:14:51 -0400

    Add an Acceptation for each LexerExpression.
    Signed-off-by: Jean Privat <jean@pryen.org>
    Signed-off-by: Etienne M. Gagnon <egagnon@j-meg.com>

--- src/org/sablecc/sablecc/core/LexerExpression.java
+++ src/org/sablecc/sablecc/core/LexerExpression.java
@@ -35,6 +35,8 @@ public abstract class LexerExpression
 
     private Automaton savedAutomaton;
 
+    private Acceptation acceptation;
+
     LexerExpression(
             Grammar grammar) {
 
@@ -148,6 +150,17 @@ public abstract class LexerExpression
     Automaton getSavedAutomaton() {
 
         return this.savedAutomaton;
+    }
+
+    /**
+     * The associated Acceptation object. Each LexerExpression is associated
+     * with a Acceptation object used to refer itself in an automaton.
+     */
+    public Acceptation getAcceptation() {
+
+        if (this.acceptation == null)
+            this.acceptation = new Acceptation(getExpressionName());
+        return this.acceptation;
     }
 
     public static class NamedExpression
