64bc8ecba4bce40f9db503954a2726e6334aae73
Lâm TRAN VAN BA <vuanhlam.tranvanba@gmail.com>
2017-11-09 23:31:32 -0500

    Add copyrights
    Signed-off-by: Lâm TRAN VAN BA <vuanhlam.tranvanba@gmail.com>

--- src/org/sablecc/objectmacro/codegeneration/java/MacroCollector.java
+++ src/org/sablecc/objectmacro/codegeneration/java/MacroCollector.java
@@ -1,3 +1,19 @@
+/* This file is part of SableCC ( http://sablecc.org ).
+ *
+ * See the NOTICE file distributed with this work for copyright information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.sablecc.objectmacro.codegeneration.java;
 
 import org.sablecc.objectmacro.codegeneration.java.macro.MMacro;
@@ -9,9 +25,6 @@ import org.sablecc.objectmacro.util.Utils;
 import java.util.LinkedList;
 import java.util.Map;
 
-/**
- * Created by lam on 05/10/17.
- */
 public class MacroCollector
         extends DepthFirstAdapter{
 
--- src/org/sablecc/objectmacro/walker/MacroReferenceCollector.java
+++ src/org/sablecc/objectmacro/walker/MacroReferenceCollector.java
@@ -1,3 +1,19 @@
+/* This file is part of SableCC ( http://sablecc.org ).
+ *
+ * See the NOTICE file distributed with this work for copyright information.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.sablecc.objectmacro.walker;
 
 import org.sablecc.objectmacro.syntax3.analysis.DepthFirstAdapter;
@@ -6,9 +22,6 @@ import org.sablecc.objectmacro.structure.Macro;
 import org.sablecc.objectmacro.structure.Param;
 import org.sablecc.objectmacro.syntax3.node.*;
 
-/**
- * Created by lam on 06/09/17.
- */
 public class MacroReferenceCollector
         extends DepthFirstAdapter {
 
