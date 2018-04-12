9234c371859ae932a192cb045754976596dd5277
Vu Anh Lam Tran Van Ba <vuanhlam.tranvanba@gmail.com>
2018-04-12 11:26:42 -0400

    Fix StringBuilder creation for indentation in the object model.
    Signed-off-by: Vu Anh Lam Tran Van Ba <vuanhlam.tranvanba@gmail.com>
    Signed-off-by: Etienne M. Gagnon <egagnon@j-meg.com>

--- src/org/sablecc/objectmacro/codegeneration/java/CodeGenerationWalker.java
+++ src/org/sablecc/objectmacro/codegeneration/java/CodeGenerationWalker.java
@@ -150,6 +150,8 @@ public class CodeGenerationWalker
 
     private MAddIndent currentAddIndent;
 
+    private List<Integer> currentlyUsed = new LinkedList<>();
+
     public CodeGenerationWalker(
             IntermediateRepresentation ir,
             File packageDirectory,
@@ -680,10 +682,17 @@ public class CodeGenerationWalker
     public void caseAIndentMacroPart(
             AIndentMacroPart node) {
 
+        this.currentlyUsed.add(this.indexBuilder);
         this.indexBuilder++;
+        //Avoid declaring insert of the same name
+        while (this.createdBuilders
+                .contains(String.valueOf(this.indexBuilder))) {
+            this.indexBuilder++;
+        }
         String index_builder = String.valueOf(this.indexBuilder);
         this.currentMacroBuilder.newInitStringBuilder(index_builder);
         this.currentAddIndent = this.currentMacroBuilder.newAddIndent();
+        this.createdBuilders.add(index_builder);
 
         //To avoid modification on indexes
         Integer tempIndexBuilder = this.indexBuilder;
@@ -698,8 +707,6 @@ public class CodeGenerationWalker
 
         this.indentations.add(this.currentAddIndent);
         this.currentAddIndent = null;
-
-        this.createdBuilders.add(index_builder);
     }
 
     @Override
@@ -707,7 +714,8 @@ public class CodeGenerationWalker
             AEndIndentMacroPart node) {
 
         String index_indent = String.valueOf(this.indexBuilder);
-        this.indexBuilder--;
+        this.indexBuilder
+                = this.currentlyUsed.remove(this.currentlyUsed.size() - 1);
         this.indentations.remove(this.indentations.size() - 1);
         this.currentMacroBuilder
                 .newIndentPart(String.valueOf(this.indexBuilder), index_indent);
